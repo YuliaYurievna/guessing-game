e083059bbef69d35f6acca96f715292d956da44a
Yuliia Poddiacha <104911480+YuliaYurievna@users.noreply.github.com>
2023-02-25 18:13:15 +0200

    feat: add a solution of the task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,28 @@
 class GuessingGame {
-    constructor() {}
+  constructor() {}
 
-    setRange(min, max) {
+  arr;
 
-    }
+  setRange(min, max) {
+    this.arr = Array.from(
+      { length: max - min + 1 },
+      (value, index) => min + index
+    );
+  }
 
-    guess() {
+  guess() {
+    const middleIndex = Math.floor(this.arr.length / 2);
+    console.log(this.arr);
+    return this.arr[middleIndex];
+  }
 
-    }
+  lower() {
+    this.arr = this.arr.splice(0, Math.floor(this.arr.length / 2) + 1);
+  }
 
-    lower() {
-
-    }
-
-    greater() {
-
-    }
+  greater() {
+    this.arr = this.arr.splice(Math.floor(this.arr.length / 2));
+  }
 }
 
 module.exports = GuessingGame;
